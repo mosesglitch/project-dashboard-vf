391c707fdc5ae86d938727745d132dee24c34f0b
mosesglitch <mosesgachuru15@gmail.com>
2025-09-11 20:20:02 +0300

    renewed doubt

--- server/mongodb.ts
+++ server/mongodb.ts
@@ -2,7 +2,7 @@ import { MongoClient, Db, Collection } from "mongodb";
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://felixjdawn:MMPvV2w5IJXyY9we@gikoshcluster.r6qxd.mongodb.net/?retryWrites=true&w=majority&appName=gikoshCluster";
 
-const DB_NAME = "project_dashboard";
+const DB_NAME = "project_dashboard_demo";
 
 if (!MONGODB_URI) {
   throw new Error("❌ MongoDB URI is not defined in environment variables!");
